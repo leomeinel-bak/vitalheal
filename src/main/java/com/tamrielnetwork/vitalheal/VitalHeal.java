98f2f2afc8f0e10af2dd6322b03186c1d5bdb80a
TamrielNetwork <contact@tamriel.me>
2022-02-15 22:55:53 +0100

    Add functionality
    - Edit README.md to fit the plugin
    - Add intial logic
    - Remove unnecessary logic

--- src/main/java/com/tamrielnetwork/vitalheal/VitalHeal.java
+++ src/main/java/com/tamrielnetwork/vitalheal/VitalHeal.java
@@ -16,51 +16,39 @@
  * along with this program. If not, see https://github.com/TamrielNetwork/VitalHeal/blob/main/LICENSE
  */
 
-package com.tamrielnetwork.vitalfly;
+package com.tamrielnetwork.vitalheal;
 
-import com.tamrielnetwork.vitalfly.commands.VitalFlyCmd;
-import com.tamrielnetwork.vitalfly.files.Messages;
-import com.tamrielnetwork.vitalfly.listeners.PlayerChangeWorld;
-import com.tamrielnetwork.vitalfly.listeners.PlayerGamemodeChange;
-import com.tamrielnetwork.vitalfly.listeners.PlayerJoin;
+import com.tamrielnetwork.vitalheal.commands.VitalHealCmd;
+import com.tamrielnetwork.vitalheal.files.Messages;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
-public final class VitalFly extends JavaPlugin {
+public final class VitalHeal extends JavaPlugin {
 
 	private Messages messages;
 
 	@Override
 	public void onEnable() {
 
-		registerListeners();
-
-		Objects.requireNonNull(getCommand("vitalfly")).setExecutor(new VitalFlyCmd());
-		Objects.requireNonNull(getCommand("vitalfly")).setTabCompleter(new VitalFlyCmd());
+		Objects.requireNonNull(getCommand("vitalheal")).setExecutor(new VitalHealCmd());
 
 		saveDefaultConfig();
 
 		messages = new Messages();
 
-		Bukkit.getLogger().info("VitalFly v" + this.getDescription().getVersion() + " enabled");
+		Bukkit.getLogger().info("VitalHeal v" + this.getDescription().getVersion() + " enabled");
 		Bukkit.getLogger().info("Copyright (C) 2022 Leopold Meinel");
 		Bukkit.getLogger().info("This program comes with ABSOLUTELY NO WARRANTY!");
 		Bukkit.getLogger().info("This is free software, and you are welcome to redistribute it under certain conditions.");
-		Bukkit.getLogger().info("See https://github.com/TamrielNetwork/VitalFly/blob/main/LICENSE for more details.");
+		Bukkit.getLogger().info("See https://github.com/TamrielNetwork/VitalHeal/blob/main/LICENSE for more details.");
 	}
 
 	@Override
 	public void onDisable() {
 
-		Bukkit.getLogger().info("VitalFly v" + this.getDescription().getVersion() + " disabled");
-	}
-
-	private void registerListeners() {
-		getServer().getPluginManager().registerEvents(new PlayerChangeWorld(), this);
-		getServer().getPluginManager().registerEvents(new PlayerGamemodeChange(), this);
-		getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+		Bukkit.getLogger().info("VitalHeal v" + this.getDescription().getVersion() + " disabled");
 	}
 
 	public Messages getMessages() {
---
+++ src/main/java/com/tamrielnetwork/vitalheal/commands/VitalHealCmd.java
@@ -0,0 +1,83 @@
+/*
+ * VitalHeal is a Spigot Plugin that gives players the ability to heal.
+ * Copyright © 2022 Leopold Meinel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://github.com/TamrielNetwork/VitalHeal/blob/main/LICENSE
+ */
+
+package com.tamrielnetwork.vitalheal.commands;
+
+import com.google.common.collect.ImmutableMap;
+import com.tamrielnetwork.vitalheal.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class VitalHealCmd implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+		// Check args length
+		if (args.length > 1) {
+			Utils.sendMessage(sender, "invalid-option");
+			return true;
+		}
+		// Toggle Crafting Interface
+		setFoodLevel(sender, args);
+		return true;
+
+	}
+
+	private void setFoodLevel(CommandSender sender, String[] args) {
+		// Check if command sender is a player
+		if (!(sender instanceof Player)) {
+			Utils.sendMessage(sender, "player-only");
+			return;
+		}
+		// Check perms
+		if (!sender.hasPermission("vitalheal.heal")) {
+			Utils.sendMessage(sender, "no-perms");
+			return;
+		}
+
+		// Check args length
+		if (args.length == 1) {
+			// Check perms
+			if (!sender.hasPermission("vitalheal.heal.others")) {
+				Utils.sendMessage(sender, "no-perms");
+				return;
+			}
+			if (Bukkit.getPlayer(args[0]) == null) {
+				Utils.sendMessage(sender, "invalid-player");
+				return;
+			}
+			Player player = Bukkit.getPlayer(args[0]);
+			boolean isOnline = Objects.requireNonNull(player).isOnline();
+			if (!isOnline) {
+				Utils.sendMessage(sender, "not-online");
+				return;
+			}
+			Utils.sendMessage(sender, ImmutableMap.of("%player%", player.getName()), "player-healed");
+			player.setHealth(20);
+			return;
+		}
+		Utils.sendMessage(sender,"healed");
+		((Player) sender).setHealth(20);
+	}
+}
